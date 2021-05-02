2da835deb394351972e35d6aa7a8331a1e16e154
Effiong Bassey <f.young.de@gmail.com>
2021-05-02 23:30:17 +0100

    Google map markers project with Typescript
    Used typescript to display a google map with 2 markers showing a randomly generated user and company details

---
+++ src/Company.ts
@@ -0,0 +1,31 @@
+import faker from 'faker'
+import { Mappable } from './CustomMap'
+
+export class Company implements Mappable {
+    companyName: string
+    catchPhrase: string
+    location: {
+        lat: number,
+        lng: number
+    }
+
+    constructor() {
+        this.companyName = faker.company.companyName()
+        this.catchPhrase = faker.company.catchPhrase()
+        this.location = {
+            lat: parseFloat( faker.address.latitude() ),
+            lng: parseFloat(faker.address.longitude())
+        }
+    }
+
+    markerContent(): string {
+        return `
+        <div>
+            <h2>Company Name: ${this.companyName}</h2>
+            <p>Catch Phrase: ${this.catchPhrase}</p>
+        </div>
+        `
+    }
+
+    color: string = 'Blue'
+}
